905132032c4508f48ede3f4655b6b793c3c4d655
Kasia Klepacka <kklepacka@henix.fr>
2024-02-01 12:41:16 +0100

    ts

--- Test Suites/Account Creation.ts
+++ Test Suites/Account Creation.ts
@@ -5,10 +5,24 @@
    <tag></tag>
    <isRerun>false</isRerun>
    <mailRecipient></mailRecipient>
-   <numberOfRerun>0</numberOfRerun>
+   <numberOfRerun>3</numberOfRerun>
    <pageLoadTimeout>30</pageLoadTimeout>
    <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
    <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
-   <rerunImmediately>false</rerunImmediately>
+   <rerunImmediately>true</rerunImmediately>
    <testSuiteGuid>3071c1e9-94a8-4977-b18b-027360b9b98e</testSuiteGuid>
+   <testCaseLink>
+      <guid>cb0d3a84-ef0c-4c74-94db-1bf5925b85af</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Cannot create an account when the email format is invalid</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>ba5ebb05-7c8b-4037-9b68-f0fb7c8d0027</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Create an account</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
 </TestSuiteEntity>
