0a82a23101e09fde3ab0035361d6e43b505a0360
Kasia Klepacka <kklepacka@henix.fr>
2024-02-01 12:29:09 +0100

    first commit

---
+++ Test Suites/Account Creation.ts
@@ -0,0 +1,14 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>Account Creation</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>false</rerunImmediately>
+   <testSuiteGuid>3071c1e9-94a8-4977-b18b-027360b9b98e</testSuiteGuid>
+</TestSuiteEntity>
